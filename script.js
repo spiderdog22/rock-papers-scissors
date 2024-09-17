7ef6cfcc948db78f508f783835769fca10b69b87
spiderdog22 <na>
2024-09-17 09:09:00 -0500

    Generate random choices

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello world!')
+
+const options = [ 'rock', 'paper', 'scissors' ]
+
+let getComputerChoice = () => {
+    let randomNumber = Math.random()
+    let module = (randomNumber * 100) % 3
+    return options[Math.trunc(module)]
+}
+
