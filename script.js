1dbac643eb2fdf95aaa502a16a863937e990dbfb
spiderdog22 <na>
2024-09-18 17:01:42 -0500

    Fix some lines of code

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
+let startButton = document.querySelector('#start-button');
+let rockButton = document.querySelector('#ctrl-rock');
+let paperButton = document.querySelector('#ctrl-paper');
+let scissorsButton = document.querySelector('#ctrl-scissors');
+let gameStatusMsg = document.querySelector('.gcs-game-status');
+let humanPointsElement = document.querySelector('#human-points');
+let computerPointsElement = document.querySelector('#computer-points');
+
+let computerPoints = 0
+let humanPoints = 0
+const MAX_POINTS = 4;
+
+let gameStatus = 'idle';
+
+const startButtonHandler = () => {
+    if (gameStatus === 'idle') {
+        setUpGameGUI();
+        gameStatus = 'playing';
+        gameStatusMsg.textContent = 'Waiting for human...';
+    } else if (gameStatus === 'playing') {
+        resetGameGui();
+        resetPoints();
+        gameStatus = 'idle';
+    }
+};
+
+const processRound = (evt) => {
+    let humanChoice = evt.target.textContent.toLowerCase();
+    let computerChoice = getComputerChoice();
+    let result = playRound(humanChoice, computerChoice);
+
+    if (result === 'you win')
+        humanPoints++;
+    else if (result === 'you lose')
+        computerPoints++;
+    
+    gameStatusMsg.textContent = getRoundMessage(result, humanChoice, computerChoice);
+    setGuiPoints(humanPoints, computerPoints);
+
+    if (humanPoints > MAX_POINTS || computerPoints > MAX_POINTS)
+        startButton.dispatchEvent(new Event('click'));
+};
+
+startButton.addEventListener('click', startButtonCallback);
+rockButton.addEventListener('click', processRound);
+paperButton.addEventListener('click', processRound);
+scissorsButton.addEventListener('click', processRound);
+
 const getComputerChoice = () => {
+
     const options = [ 'rock', 'paper', 'scissors' ]
     let randomNumber = Math.random()
     let module = (randomNumber * 100) % 3
@@ -44,59 +93,10 @@ const getRoundMessage = (result, humanChoice, computerChoice) => {
     return 'Unknown result!';
 };
 
-let startButton = document.querySelector('#start-button');
-let rockButton = document.querySelector('#ctrl-rock');
-let paperButton = document.querySelector('#ctrl-paper');
-let scissorsButton = document.querySelector('#ctrl-scissors');
-let gameStatusMsg = document.querySelector('.gcs-game-status');
-let humanPointsElement = document.querySelector('#human-points');
-let computerPointsElement = document.querySelector('#computer-points');
-
-let gameStatus = 'idle';
-
-const startButtonCallback = () => {
-    if (gameStatus === 'idle') {
-        setUpGameGUI();
-        gameStatus = 'playing';
-        gameStatusMsg.textContent = 'Waiting for human...';
-    } else if (gameStatus === 'playing') {
-        resetGameGui();
-        resetPoints();
-        gameStatus = 'idle';
-    }
-};
-
-let computerPoints = 0
-let humanPoints = 0
-const MAX_POINTS = 4;
-
 const resetPoints = () => {
     humanPoints = 0
     computerPoints = 0
 }
-
-const processRound = (evt) => {
-    let humanChoice = evt.target.textContent.toLowerCase();
-    let computerChoice = getComputerChoice();
-    let result = playRound(humanChoice, computerChoice);
-
-    if (result === 'you win')
-        humanPoints++;
-    else if (result === 'you lose')
-        computerPoints++;
-    
-    gameStatusMsg.textContent = getRoundMessage(result, humanChoice, computerChoice);
-    setGuiPoints(humanPoints, computerPoints);
-
-    if (humanPoints > MAX_POINTS || computerPoints > MAX_POINTS) {
-        startButton.dispatchEvent(new Event('click'));
-    }
-};
-
-startButton.addEventListener('click', startButtonCallback);
-rockButton.addEventListener('click', processRound);
-paperButton.addEventListener('click', processRound);
-scissorsButton.addEventListener('click', processRound);
 
 const setUpGameGUI = () => {
     startButton.textContent = 'Reset';
